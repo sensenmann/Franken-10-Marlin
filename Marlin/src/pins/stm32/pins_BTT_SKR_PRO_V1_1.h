e166552ada8ed7eec279ba40be16007fe1cbb42e
René Huber <rene.huber@rhse.at>
2020-01-28 03:14:56 +0100

    Enabled EEPROM for SKR PRO Board

--- Marlin/src/pins/stm32/pins_BTT_SKR_PRO_V1_1.h
+++ Marlin/src/pins/stm32/pins_BTT_SKR_PRO_V1_1.h
@@ -31,7 +31,7 @@
 
 // Use one of these or SDCard-based Emulation will be used
 //#define SRAM_EEPROM_EMULATION   // Use BackSRAM-based EEPROM emulation
-//#define FLASH_EEPROM_EMULATION  // Use Flash-based EEPROM emulation
+#define FLASH_EEPROM_EMULATION  // Use Flash-based EEPROM emulation
 
 //
 // Servos
